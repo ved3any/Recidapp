a04383f2d2647d454430ff77a012458c95cb1bab
Pallavi Chaudhari <vedantkaushik212@gmail.com>
2025-01-24 23:52:02 +0530

    initial commit

---
+++ src/main/java/com/example/demo/RecidAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecidAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecidAppApplication.class, args);
+	}
+
+}
